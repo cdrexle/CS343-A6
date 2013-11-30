d9e68579e29308458f99262a36a609ab37cb2342
c2drexle <drexlerlemirec@gmail.com>
2013-11-30 02:01:11 -0500

    Chris - Vending Machine

--- VendingMachine.cc
+++ VendingMachine.cc
@@ -1,33 +1,66 @@
 #include "VendingMachine.h"
 #include "Printer.h"
 #include "NameServer.h"
+#include "WATCard.h"
 
 void VendingMachine::main()
 {
-
+	stock = new unsigned int[4];
+	for (unsigned int i = 0; i < 4; i++) {
+		stock[i] = 0;
+	}
+	nameServer->VMregister(this);
+	while (true) {
+		_Accept(~VendingMachine) {}
+		or _When(!beingStocked) _Accept(inventory) {
+			beingStocked = true;
+		}
+		or _When(beingStocked) _Accept(restocked) {
+			beingStocked = false;
+		}
+		or _When(!beingStocked) _Accept(buy) {}
+	}
 }
 VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
-                    unsigned int maxStockPerFlavour )
-{
+                    unsigned int maxStockPerFlavour ) :
+	prt(&prt),
+	nameServer(&nameServer),
+	id(id),
+	sodaCost(sodaCost),
+	maxStockPerFlavour(maxStockPerFlavour),
+	beingStocked(false)
+{}
 
+VendingMachine::~VendingMachine() {
+	delete[] stock;
 }
+
 VendingMachine::Status VendingMachine::buy( Flavours flavour, WATCard &card )
 {
-
+	if (stock[flavour] == 0) return STOCK;
+	// There's at least one soda for the student
+	
+	if (card.getBalance() < sodaCost) return FUNDS;
+	// balance >= sodaCost
+	
+	stock[flavour] -= 1;
+	card.withdraw(sodaCost);
+	return BUY;
 }
+
 unsigned int *VendingMachine::inventory()
 {
-
+	return stock;
 }
-void VendingMachine::restocked()
-{
 
-}
+void VendingMachine::restocked() {}
+
 _Nomutex unsigned int VendingMachine::cost()
 {
-
+	return sodaCost;
 }
+
 _Nomutex unsigned int VendingMachine::getId()
 {
-    
+    return id;
 }
--- VendingMachine.h
+++ VendingMachine.h
@@ -12,11 +12,20 @@ _Task VendingMachine {
     enum Status { BUY, STOCK, FUNDS };     // purchase status: successful buy, out of stock, insufficient funds
     VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour );
+    ~VendingMachine();
     Status buy( Flavours flavour, WATCard &card );
     unsigned int *inventory();
     void restocked();
     _Nomutex unsigned int cost();
     _Nomutex unsigned int getId();
+  private:
+  	unsigned int* stock;
+  	Printer* prt;
+	NameServer* nameServer;
+	unsigned int id;
+	unsigned int sodaCost;
+	unsigned int maxStockPerFlavour;
+	bool beingStocked;
 };
 
 #endif
